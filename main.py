772d5143605f2687a09b5e1f09300c1f00c6bce9
Damian Piatkowski <piatkowski.damian@outlook.com>
2021-02-14 14:54:15 +0100

    Added validate_date function Started collect_user_input

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-def collect_new_row():
-    #code
+from datetime import datetime
+
+def validate_date(user_input: str) -> bool:
+    try:
+        datetime.strptime(user_input, "%d/%m/%Y")
+        return True
+    except ValueError:
+        print("Incorrect date format, should be dd/mm/yyyy")
+        return False
+
+def collect_user_input(categories: list) -> dict:
+    new_row = dict()
+    while True:
+        new_row["date"] = input("What's the date of this purchase? Please use the format dd/mm/yyyy")
+        if validate_date(new_row["date"]):
+            break
+        else:
+            print("Try again")
 
 def main():
     #your main flow, call other functions here
+    print("nothing")
 
 if __name__ == "__main__":
     main()
