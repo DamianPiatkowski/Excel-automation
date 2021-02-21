3284d038a797881065abc1f73517a65ff24bac22
Damian Piatkowski <piatkowski.damian@outlook.com>
2021-02-21 21:11:52 +0100

    create_new_excel function created. save_new_rows_to_excel function created too.

--- excel_shopping_tracker.py
+++ excel_shopping_tracker.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import PatternFill
+import os
 
+file_path = os.path.join(os.environ['USERPROFILE'], 'Desktop/Finances.xlsx')
 categories = ["baby", "regular groceries", "game", "car related", "taxi"]
 
 def validate_date(user_input: str) -> bool:
@@ -22,8 +26,9 @@ def collect_user_input(categories: list) -> list:
                 print("Wrong format, try again")
 
         while True:
-            row["amount"] = input("What's the amount?")
-            if row["amount"].isdigit():
+            amount = input("What's the amount?")
+            if amount.isdigit():
+                row["amount"] = int(amount)
                 break
             else:
                 print("Please give just the number")
@@ -50,7 +55,39 @@ def collect_user_input(categories: list) -> list:
         if another_one == "no":
             print("cool")
             return new_rows
+
+
+def create_new_excel():
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet["A1"] = "Date"
+    sheet["B1"] = "Amount"
+    sheet["C1"] = "Category"
+    sheet["D1"] = "Description"
+    for cell in ["A1", "B1", "C1", "D1"]:
+        sheet[cell].fill = PatternFill(start_color="00FF00", patternType="solid")
+    workbook.save(filename=file_path)
+    workbook.close()
+
+def save_new_rows_to_excel(rows: list):
+    workbook = load_workbook(filename=file_path)
+    sheet = workbook.active
+    current_index = sheet.max_row + 1
+    for row in rows:
+        sheet["A"+str(current_index)] = datetime.strptime(row['date'], "%d/%m/%Y").date()
+        sheet["B"+str(current_index)] = row['amount']
+        sheet["C"+str(current_index)] = row['category']
+        sheet["D"+str(current_index)] = row['description']
+        current_index +=1
+    
+    workbook.save(filename=file_path)
+
 def main():
+    save_new_rows_to_excel([
+                        {'date': '10/01/2021', 'amount': 100, 'category': 'car related', 'description': 'fuel'},
+                        {'date': '12/01/2021', 'amount': 23, 'category': 'game', 'description': 'Mario'}, 
+                        {'date': '14/01/2021', 'amount': 455, 'category': 'groceries', 'description': 'Auchan'}
+                    ])
     print(collect_user_input(categories))
 
 if __name__ == "__main__":
