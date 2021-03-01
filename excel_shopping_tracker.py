1efe2f61ae11acf4afe10e3f6fd6cbe7fa347c5a
Damian Piatkowski <piatkowski.damian@outlook.com>
2021-03-01 21:45:02 +0100

    Progress on get_stats_data function.

--- excel_shopping_tracker.py
+++ excel_shopping_tracker.py
@@ -19,41 +19,43 @@ def collect_user_input(categories: list) -> list:
     while True:
         row = dict()
         while True:
-            row["date"] = input("What's the date of this purchase? Please use the format dd/mm/yyyy")
+            row["date"] = input("What's the date of this purchase? Please use the format dd/mm/yyyy ")
             if validate_date(row["date"]):
                 break
             else:
                 print("Wrong format, try again")
 
         while True:
-            amount = input("What's the amount?")
+            amount = input("What's the amount? ")
             if amount.isdigit():
                 row["amount"] = int(amount)
                 break
             else:
-                print("Please give just the number")
+                print("Please give just the number ")
         
         while True:
             for count, value in enumerate(categories, start=1):
                 print(count, value)
-            index = int(input("Choose the category by writing its number"))
+            index = int(input("Choose the category by writing its number "))
             if 1 <= int(index) <= len(categories):
                 row["category"] = categories[index-1]
                 break
             else:
-                print("Please choose one of the available numbers")
+                print("Please choose one of the available numbers ")
         
-        row["description"] = input("Add a short description for this purchase")
+        row["description"] = input("Add a short description for this purchase ")
         new_rows.append(row)
         
         while True:
-            another_one = input("Done, would you like to add another purchase? yes/no")
+            another_one = input("Done, would you like to add another purchase? yes/no ")
             if another_one.lower() in ["yes", "no"]:
                 break
             else:
                 print("Something went wrong, answer yes or no")
         if another_one == "no":
-            print("cool")
+            print(f"cool, the following {len(new_rows)} row(s) will be saved:")
+            for row in new_rows:
+                print(row)
             return new_rows
 
 
@@ -101,7 +103,7 @@ def get_user_request(stats_options: list) -> list:
         print(option)
     user_choices = []
     while True:
-        answer = input("Would you like to see statistics for any of these months? yes/no")
+        answer = input("Would you like to see statistics for any of these months? yes/no ")
         if answer.lower() == "no":
             return user_choices
         elif answer.lower() == "yes":
@@ -111,7 +113,7 @@ def get_user_request(stats_options: list) -> list:
     
     while True:
         while True:
-            option_chosen = input("Write what option in format mm/YY, for example 01/20 for January 2020")
+            option_chosen = input("Write what option in format mm/YY, for example 01/20 for January 2020 ")
             if option_chosen in stats_options and option_chosen not in user_choices:
                 user_choices.append(option_chosen)
                 print(option_chosen + "added to your request")
@@ -125,7 +127,7 @@ def get_user_request(stats_options: list) -> list:
             print("So far you have requested statistics for these months:")
             for option in user_choices:
                 print(option)
-            want_quit = input("Would you like to add another month to your request? yes/no")
+            want_quit = input("Would you like to add another month to your request? yes/no ")
             if want_quit.lower() == "no":
                 return user_choices
             elif want_quit.lower() == "yes":
@@ -134,16 +136,47 @@ def get_user_request(stats_options: list) -> list:
                 print("Something went wrong, try again: yes/no")
     return user_choices    
 
+def get_stats_data(categories: list, user_choices: list) -> list:
+    workbook = load_workbook(filename=file_path)
+    sheet = workbook.active
+    stats_data = []
+    for month in user_choices:
+        new_dict = {}
+        new_dict["month"] = month
+        new_dict["total"] = 0
+        all_transactions = []
+        for row in sheet.iter_rows(min_row=2,min_col=1, max_col=4, values_only=True):
+            if month == datetime.strftime(row[0], "%m/%y"):
+                new_dict["total"] += row[1]
+                all_transactions.append(row)
+        all_transactions.sort(key=lambda x: x[1], reverse=True)
+        new_dict["top_5"] = all_transactions[:5]
+        new_dict["categories"] = {}
+        for category in categories:
+            new_dict["categories"][category] = sum([i[1] for i in all_transactions if i[2] == category])
+        print(new_dict)
+        
+
+
+
+
+#def print_stats(stats_data: list):
+
+#def plot(stats_data):
+
 def main():
-    save_new_rows_to_excel([
-                        {'date': '10/01/2021', 'amount': 100, 'category': 'car related', 'description': 'fuel'},
-                        {'date': '12/01/2021', 'amount': 23, 'category': 'game', 'description': 'Mario'}, 
-                        {'date': '14/01/2021', 'amount': 455, 'category': 'groceries', 'description': 'Auchan'}
-                    ])
+    new_rows = collect_user_input(categories)
+    if os.path.isfile(file_path) == False:
+        create_new_excel()
+    save_new_rows_to_excel(new_rows)
     stats_options = get_stats_options()
     user_choices = get_user_request(stats_options)
-    print(user_choices)
-    #print(collect_user_input(categories))
+    if user_choices != []:
+        stats_data = get_stats_data(categories, user_choices)
+        #print_stats(stats_data)
+        #if len(user_choices) > 1:
+            #plot(user_choices)
+    input("Hit the enter to exit. Thanks!")
 
 if __name__ == "__main__":
     main()
